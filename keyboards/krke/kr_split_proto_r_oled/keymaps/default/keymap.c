a1f5015507d1214c070e0ce717a252c73ba4a09c
Ant <46870585+DoesItRlyMatter@users.noreply.github.com>
2024-06-26 03:09:02 +0300

    more oled code (animations)

--- keyboards/krke/kr_split_proto_r_oled/keymaps/default/keymap.c
+++ keyboards/krke/kr_split_proto_r_oled/keymaps/default/keymap.c
@@ -1,10 +1,18 @@
 #include QMK_KEYBOARD_H
 #include "print.h"
+#include "bongo.c"
+#include <stdio.h>
+
+#define CUSTOM_OLED_TIMEOUT 300000 // (after 5 minutes)
+
+// Variable for WPM Counter
+char wpm[5];
 
 // LAYERS
 enum layers {
     _BL = 0,
-    _FL
+    _FL,
+    _EX
 };
 
 //  CUSTOM KEYCODES. KC_NONE IS PLACEHOLDER.
@@ -18,11 +26,12 @@ enum custom_keycodes {
 enum oled_layers {
     OL_LOGO = 0,
     OL_INFO,
-    OL_EXT
+    OL_ANIM,
+    NUM_OF_PAGES // DO NOT REMOVE, USED FOR COUNTING ENUMS.
 };
 
-// OLED LAYER
-uint16_t current_oled_layer = 0;
+// OLED LAYER. START AT DEFINED, WOULD TYPICALLY BE 0.
+uint16_t current_oled_layer = 1;
 
 // JIGGLER STUFF
 __attribute__((weak))
@@ -56,91 +65,110 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MPLY,    MO(_FL)
     ),
     [_FL] = LAYOUT_normal(
-        QK_BOOT,    DB_TOGG,
+        TO(_EX),    QK_BOOT,
         KC_OLM,     KC_OLP,
         KC_JIGG,    KC_TRNS
+    ),
+    [_EX] = LAYOUT_normal(
+        TO(_BL),    KC_1,
+        KC_A,       KC_B,
+        KC_SPC,     KC_BSPC
     )
 };
 
 // OLED
 #ifdef OLED_ENABLE
+
 bool oled_task_user(void) {
-
-    // QMK Logo
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_clear();
-    // Oled pages
-    switch(current_oled_layer){
-        case OL_LOGO:
-            // Write logo & kb information
-            oled_write_ln_P(qmk_logo, false);
-            oled_write_P(PSTR(" KRKE\n"), false);
-            oled_write_P(PSTR(" KR-Split-Proto\n"), false);
-            oled_write_P(PSTR(" Version: 0.0.1 OLED\n"), false);
-            // DEBUG
-            dprintf("CUR_OLED_LAYER: %d \n", current_oled_layer);
-            break;
-        case OL_INFO:
-            // Layer & Indicators
-            oled_write_P(PSTR("Layer: "), false);
-            switch (get_highest_layer(layer_state)) {
-                case _BL:
-                    oled_write_P(PSTR("_BL\n"), false);
-                    // DEBUG
-                    dprintf("CUR_OLED_LAYER: %d \n", current_oled_layer);
-                    break;
-                case _FL:
-                    oled_write_P(PSTR("_FL\n"), false);
-                    // DEBUG
-                    dprintf("CUR_OLED_LAYER: %d \n", current_oled_layer);
-                    break;
-                default:
-                    oled_write_P(PSTR("Undefined\n"), false);
-                    break;
-            }
-            // Jiggler
-            oled_write_P(PSTR("\nJiggler: "), false);
-            if(is_jiggling){
-                oled_write_P(PSTR("Active  \n\n"), false);
-            }
-            else{
-                oled_write_P(PSTR("Inactive\n\n"), false);
-            }
-
-            // Host Keyboard LED Status
-            led_t led_state = host_keyboard_led_state();
-            oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-            oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-            oled_write_P(led_state.scroll_lock ? PSTR("SCR \n") : PSTR("    \n"), false);
-            // IMPORTANT BREAK!
-            break;
-        case OL_EXT:
-            oled_write_P(PSTR("TESTING"), false);
-            // DEBUG
-            dprintf("CUR_OLED_LAYER: %d \n", current_oled_layer);
-            break;
-        default:
-            oled_write_P(PSTR("Undefined"), false);
-            // DEBUG
-            dprintf("CUR_OLED_LAYER: %d \n", current_oled_layer);
+    // Check if oled is on and there has been activity.
+    if (last_input_activity_elapsed() > CUSTOM_OLED_TIMEOUT){
+        // Turn off if oled on.
+        if (is_oled_on()){
+            oled_off();
+        }
     }
-    oled_render_dirty(true);
+    // if oled is off and theres activity
+    else {
+        // turn oled on
+        if(!is_oled_on()){
+            oled_on();
+        }
+        // QMK Logo
+        static const char PROGMEM qmk_logo[] = {
+            0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+            0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+            0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+        };
+
+        oled_clear();
+        // Oled pages
+        switch(current_oled_layer){
+            case OL_LOGO:
+                // Write logo & kb information
+                oled_write_P(PSTR("\n"), false);
+                oled_write_ln_P(qmk_logo, false);
+                oled_write_P(PSTR(" KRKE\n"), false);
+                oled_write_P(PSTR(" KR-Split-Proto\n"), false);
+                oled_write_P(PSTR(" Version: 0.0.1 OLED\n"), false);
+                break;
+            case OL_INFO:
+                // Layer & Indicators
+                oled_write_P(PSTR("\nLayer: "), false);
+                switch (get_highest_layer(layer_state)) {
+                    case _BL:
+                        oled_write_P(PSTR(" Base\n"), false);
+                        break;
+                    case _FL:
+                        oled_write_P(PSTR(" Func\n"), false);
+                        break;
+                    case _EX:
+                        oled_write_P(PSTR(" Test\n"), false);
+                        break;
+                    default:
+                        oled_write_P(PSTR("Undefined\n"), false);
+                        break;
+                }
+                // Jiggler
+                oled_write_P(PSTR("\nJiggle: "), false);
+                if(is_jiggling){
+                    oled_write_P(PSTR("Active  \n\n\n"), false);
+                }
+                else{
+                    oled_write_P(PSTR("Inactive\n\n\n"), false);
+                }
+
+                // Host Keyboard LED Status
+                led_t led_state = host_keyboard_led_state();
+                oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+                oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+                oled_write_P(led_state.scroll_lock ? PSTR("SCR \n") : PSTR("    \n"), false);
+                // IMPORTANT BREAK!
+                break;
+            case OL_ANIM:
+                // Call anime animation.
+                render_bongo();
+                // Display WPM
+                oled_write_P(PSTR("\n WPM: "), false);
+                // WPM to string
+                sprintf(wpm, "%i", get_current_wpm());
+                oled_write(wpm, false);
+                break;
+            default:
+                oled_write_P(PSTR("Undefined"), false);
+        }
+        oled_render_dirty(true);
+    }
     return false;
 }
 #endif
 
 // debug
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=false;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
+    // Customise these values to desired behaviour
+    // debug_enable=false;
+    // debug_matrix=true;
+    // debug_keyboard=true;
+    // debug_mouse=true;
 }
 
 // CUSTOM KEYCODES.
@@ -159,15 +187,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case KC_OLP:
             if (record->event.pressed) {
-                if (current_oled_layer < OL_EXT){
+                if (current_oled_layer < NUM_OF_PAGES-1){
                     current_oled_layer = current_oled_layer + 1;
+                }
+                else {
+                    current_oled_layer = 0; // Goto first page.
                 }
             }
             break;
         case KC_OLM:
             if (record->event.pressed) {
-                if (current_oled_layer > OL_LOGO) {
+                if (current_oled_layer > 0) {
                     current_oled_layer = current_oled_layer - 1;
+                }
+                else {
+                    current_oled_layer = NUM_OF_PAGES-1; // Goto last page
                 }
             }
             break;
--- keyboards/krke/kr_split_proto_r_oled/rules.mk
+++ keyboards/krke/kr_split_proto_r_oled/rules.mk
@@ -1,8 +1,6 @@
-# This file intentionally left blank
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no       	# Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      	# Enable keyboard backlight functionality
@@ -10,8 +8,19 @@ RGBLIGHT_ENABLE = no        # Enable RGB underglow
 ENCODER_ENABLE = no			# Enable encoder
 ENCODER_MAP_ENABLE = no		# Enable encoder mapping
 AUDIO_ENABLE = no           # Audio output
+
+# Enabled while debugging, can be disabled later.
+CONSOLE_ENABLE = no
+
+# Slower & smaller compile.
 LTO_ENABLE = yes
 
+# Deffered exec for jiggler.
+DEFERRED_EXEC_ENABLE = yes
+
+# WPM for bongo animation.
+WPM_ENABLE = yes
+
+# Either split or oled, both cannot be used at same time.
 SPLIT_KEYBOARD = no
 OLED_ENABLE = yes
-DEFERRED_EXEC_ENABLE = yes
